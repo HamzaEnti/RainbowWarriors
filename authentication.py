5fbb0b0f63fa6edd62ff59e6e1d577ddd25330d1
HamzaEnti <hamza.elhaddad@enti.cat>
2024-11-26 10:17:30 +0000

    base_programa(v.1)

--- authentication.py
+++ authentication.py
@@ -1,4 +1,28 @@
+import getpass
+
 # authentication.py
-def authenticate_user():
-    # Autenticar al usuario, pidiendo la contraseña maestra
-    pass
+def login():    
+    user = input("Username: ")
+    passw = getpass.getpass("Password: ")
+    f = open("users.txt", "r")
+    for line in f.readlines():
+        us, pw = line.strip().split("|")
+        if (user in us) and (passw in pw):
+            print ("Login successful!")
+            return True
+    print ("Wrong username/password")
+    return False
+
+def menu():
+    print("Menu")
+    #Aqui va el menu 
+
+def main():
+    while True:
+        log = login()
+        if log == True:
+             menu()
+             break
+
+
+main()
