ea3e151bfba45d81cfc2be302448fa25f3fc0cb5
Nico <nicolau.mesalles@enti.cat>
2024-11-27 10:49:24 +0100

    pass_generator(v3)

--- pass_generator.py
+++ pass_generator.py
@@ -1,13 +1,11 @@
-# Ho ha de fer el Nico
 # pass_generator.py
 import secrets
 import string
 
-
+#Aixo fa la contrasenya.
 def pass_generator():
-    num = int(input("Length:"))
+    num = int(input("Length:"))# Això marca la llargada de la contrasenya. En principu a la GUI no es pot escollir res que no sigui numero, per lo tant no s'ha de fer error management. 
     alfabet = string.ascii_letters + string.digits
-    password = "" .join(secrets.choice(alfabet) for char in range(num))
-    return print(password)
-
-pass_generator()
+    GENERATEDPASSWORD = "" .join(secrets.choice(alfabet) for char in range(num))
+    return GENERATEDPASSWORD #aquesta variable no es pot canviar
+    
