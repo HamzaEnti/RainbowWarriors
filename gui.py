4d306c309127dec5c38c0236e28e2dea1567c89d
HamzaEnti <hamza.elhaddad@enti.cat>
2024-11-27 11:20:46 +0100

    gui(v2)

--- gui.py
+++ gui.py
@@ -3,22 +3,24 @@ import os
 
 def display_ui():
     root = tk.Tk()
-    root.title("Authentication")
-    root.geometry("350x300")
+    root.title("RainbowWarriors Pass Manager")
+    root.geometry("450x400")
     root.config(bg="#f5f5f5")  # Fondo más agradable
+    root.iconbitmap("myIcon.ico")
 
+    
     logo = tk.PhotoImage(file="logo.png")
-    logo = logo.subsample(13, 13)  # Redimensiona el logo (reduce el tamaño)
+    logo = logo.subsample(4, 4)  # Redimensiona el logo (reduce el tamaño)
 
     logo_label = tk.Label(root, image=logo, bg="#f5f5f5")
     logo_label.image = logo  # Mantener referencia
     logo_label.pack(pady=10)
 
-    tk.Label(root, text="Usuari:", font=("Arial", 12), bg="#f5f5f5").pack(pady=5)
+    tk.Label(root, text="Username:", font=("Arial", 12), bg="#f5f5f5").pack(pady=5)
     username_entry = tk.Entry(root, font=("Arial", 12), width=25)
     username_entry.pack(pady=5)
 
-    tk.Label(root, text="Contrasenya:", font=("Arial", 12), bg="#f5f5f5").pack(pady=5)
+    tk.Label(root, text="Password:", font=("Arial", 12), bg="#f5f5f5").pack(pady=5)
     password_entry = tk.Entry(root, show="*", font=("Arial", 12), width=25)
     password_entry.pack(pady=5)
 
