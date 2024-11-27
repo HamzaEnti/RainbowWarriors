96e5334ee4f18411e47214f66b717663fb828753
HamzaEnti <hamza.elhaddad@enti.cat>
2024-11-27 11:29:25 +0100

    authentication(v3)

--- authentication.py
+++ authentication.py
@@ -1,30 +1,10 @@
-import getpass
-
-# authentication.py
-def login():    
-    user = input("Username: ")
-    passw = getpass.getpass("Password: ") #getpass pilla la contraseña, et dema un input i quan ho escrius no es veu exemple; Password: (123) lo que esta entre parentesis no es mostra en pantalla pero ho has escrit
-    f = open("users.txt", "r") #Obre la base de dades, en un futur haurem de fer que en canvi de obrir un .txt que sobri una bdd
-    # tota la funcio d'abaix el que fa es llegir el txt i comprova que estas escrivint be la password i username, quan sigui en bdd shaura de canviar
-    for line in f.readlines():
-        us, pw = line.strip().split("|")
-        if (user in us) and (passw in pw):
-            print ("Login successful!")
-            return True
-    print ("Wrong username/password")
+def login(username, password):
+    """
+    Valida las credenciales del usuario desde un archivo de texto.
+    """
+    with open("users.txt", "r") as f:
+        for line in f.readlines():
+            us, pw = line.strip().split("|")
+            if username == us and password == pw:
+                return True
     return False
-
-def menu():
-    print("Menu")
-    #Aqui va el menu 
-
-#comprova tot i fa que entris al menu
-def main():
-    while True:
-        log = login()
-        if log == True:
-             menu()
-             break
-
-
-main()
