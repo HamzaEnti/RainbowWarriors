ea3dfed811ebf239f18214df80c5d83e00cf9bdd
oscarsv-bit <oscar.saborido@enti.cat>
2024-12-03 10:58:01 +0100

    pass_encryption(v5)

--- pass_encryption.py
+++ pass_encryption.py
@@ -1,19 +1,45 @@
+import subprocess
+import sys
+
+# Comprobar e instalar pycryptodome si no esta instalado.(biblioteca de Python diseñada para realizar operaciones criptográficas)
+def install_pycryptodome():
+    try:
+        import Crypto
+    except ImportError:
+        print("La biblioteca 'pycryptodome' no está instalada. Procediendo con la instalación...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "pycryptodome"])
+        print("Instalación completada. Reinicia el programa si encuentras algún problema.")
+
 from pass_generator import pass_generator2
 from general_storage import usuari, servei
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import base64
+from Crypto.Protocol.KDF import PBKDF2
+import os
 
-
-def xor_encrypt_decrypt(input_string, KEY):
-    return ''.join(chr(ord(char) ^ KEY) for char in input_string) #Cifra o descifra una cadena usando XOR con una clave.
-
+def aes_encrypt(data, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
+    return base64.b64encode(cipher.iv + ct_bytes).decode('utf-8')
 
 def pass_storage():
-    KEY = 1
-    SERVEI = servei().strip() 
-    USUARI = usuari().strip()  
+    # Solicitar la clave al usuario
+    password = input("Introduce una contraseña para cifrar los datos: ").strip()
+    
+    # Derivar una clave AES segura a partir de la contraseña del usuario
+    salt = os.urandom(16)  # Generar un "salt" aleatorio para aumentar seguridad
+    key = PBKDF2(password, salt, dkLen=16, count=100000)  # Derivar clave de 16 bytes
+    
+    SERVEI = servei().strip()
+    USUARI = usuari().strip()
     GENERATEDPASSWORD = pass_generator2().strip()
-    ENCRYPTED_USER = xor_encrypt_decrypt(USUARI, KEY)
-    ENCRYPTED_PASSWORD = xor_encrypt_decrypt(GENERATEDPASSWORD, KEY)
-    ENCRYPTED_SERVEI = xor_encrypt_decrypt(SERVEI, KEY)
+    
+    ENCRYPTED_USER = aes_encrypt(USUARI, key)
+    ENCRYPTED_PASSWORD = aes_encrypt(GENERATEDPASSWORD, key)
+    ENCRYPTED_SERVEI = aes_encrypt(SERVEI, key)
+    
     with open("storage.txt", 'a') as archivo:
-        archivo.write(f"Servei: {ENCRYPTED_SERVEI}, Usuari: {ENCRYPTED_USER}, Contrasenya: {ENCRYPTED_PASSWORD}\n")
+        archivo.write(f"Salt: {base64.b64encode(salt).decode('utf-8')}, Servei: {ENCRYPTED_SERVEI}, Usuari: {ENCRYPTED_USER}, Contrasenya: {ENCRYPTED_PASSWORD}\n")
+
 pass_storage()
