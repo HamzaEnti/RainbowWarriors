6ba029a7847f313f481e50d87de5e17777263996
oscarsv-bit <oscar.saborido@enti.cat>
2024-12-03 11:31:33 +0100

    pass_encryption(v7)

--- pass_encryption.py
+++ pass_encryption.py
@@ -10,6 +10,7 @@ def install_pycryptodome():
         subprocess.check_call([sys.executable, "-m", "pip", "install", "pycryptodome"])
         print("Instalación completada. Reinicia el programa si encuentras algún problema.")
 install_pycryptodome()
+
 from pass_generator import pass_generator2
 from general_storage import usuari, servei
 from Crypto.Cipher import AES
@@ -24,22 +25,20 @@ def aes_encrypt(data, key):
     return base64.b64encode(cipher.iv + ct_bytes).decode('utf-8')
 
 def pass_storage():
-    # Solicitar la clave al usuario
     password = input("Introduce una contraseña para cifrar los datos: ").strip()
-    
-    # Derivar una clave AES segura a partir de la contraseña del usuario
-    salt = os.urandom(16)  # Generar un "salt" aleatorio para aumentar seguridad
-    key = PBKDF2(password, salt, dkLen=16, count=100000)  # Derivar clave de 16 bytes
-    
+    salt = os.urandom(16)  # Generar un salt único
+    key = PBKDF2(password, salt, dkLen=16, count=100000)  # Derivar la clave
+
     SERVEI = servei().strip()
     USUARI = usuari().strip()
     GENERATEDPASSWORD = pass_generator2().strip()
-    
+
     ENCRYPTED_USER = aes_encrypt(USUARI, key)
     ENCRYPTED_PASSWORD = aes_encrypt(GENERATEDPASSWORD, key)
     ENCRYPTED_SERVEI = aes_encrypt(SERVEI, key)
-    
+
     with open("storage.txt", 'a') as archivo:
-        archivo.write(f"Salt: {base64.b64encode(salt).decode('utf-8')}, Servei: {ENCRYPTED_SERVEI}, Usuari: {ENCRYPTED_USER}, Contrasenya: {ENCRYPTED_PASSWORD}\n")
+        # Guardar salt y datos cifrados en una sola línea
+        archivo.write(f"{base64.b64encode(salt).decode('utf-8')},{ENCRYPTED_SERVEI},{ENCRYPTED_USER},{ENCRYPTED_PASSWORD}\n")
 
 pass_storage()
