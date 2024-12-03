ca6eb4b02b2bad5aaac2541f5327b2d09533bb9f
Nico <nicolau.mesalles@enti.cat>
2024-12-03 11:44:54 +0100

    inside_storage.py

---
+++ inside_storage.py
@@ -0,0 +1,19 @@
+# pass_storage.py
+def store_password(name, password):
+    # Aquí va part del codig del oscar.
+    pass
+
+def get_passwords():
+    # Obtener todas las contraseñas almacenadas
+    pass
+
+def edit_password(name, new_password):
+    # Editar una contraseña almacenada
+    pass
+
+def delete_password(name):
+    # Eliminar una contraseña almacenada
+    pass
+
+
+        
