4b3db32b665b86346f1f6519a783b2afe58cc58e
Nico <nicolau.mesalles@enti.cat>
2024-12-03 12:01:33 +0100

    Update inside_storage.py

--- inside_storage.py
+++ inside_storage.py
@@ -1,10 +1,22 @@
 # pass_storage.py
 import passwod from pass_encryption
-def store_password(name, password):
-     with open("storage.txt", 'a') as archivo:
+def store_password():
+     password = input("Introduce una contraseña para cifrar los datos: ").strip()
+    salt = os.urandom(16)  # Generar un salt único
+    key = PBKDF2(password, salt, dkLen=16, count=100000)  # Derivar la clave
+
+    SERVEI = servei().strip()
+    USUARI = usuari().strip()
+    GENERATEDPASSWORD = pass_generator2().strip()
+
+    ENCRYPTED_USER = aes_encrypt(USUARI, key)
+    ENCRYPTED_PASSWORD = aes_encrypt(GENERATEDPASSWORD, key)
+    ENCRYPTED_SERVEI = aes_encrypt(SERVEI, key)
+
+    with open("storage.txt", 'a') as archivo:
         # Guardar salt y datos cifrados en una sola línea
         archivo.write(f"{base64.b64encode(salt).decode('utf-8')},{ENCRYPTED_SERVEI},{ENCRYPTED_USER},{ENCRYPTED_PASSWORD}\n")
-    pass
+
 def get_passwords():
     # Obtener todas las contraseñas almacenadas
     pass
