deecae1a4008b000cbe8cb4eb753154cd2103960
Nico <nicolau.mesalles@enti.cat>
2024-12-10 10:46:43 +0100

    pass_manager.py

--- pass_manager.py
+++ pass_manager.py
@@ -1,9 +1,9 @@
 # pass_manager.py
-from password_storage import store_password, get_passwords, delete_password, edit_password
+from inside_storage import storage_password, get_passwords, delete_password, edit_password
 from password_encryption import encrypt_password, decrypt_password
 from password_generator import generate_secure_password
 from authentication import authenticate_user
-from user_interface import display_ui
+from gui import display_ui
 
 def main():
     # Aquí se puede implementar el flujo principal de la aplicación
