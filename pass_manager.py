05d47a1a31535ea4ec96460fcf9192051a02220e
Nico <nicolau.mesalles@enti.cat>
2024-12-10 11:07:41 +0100

    pass_manager.py

--- pass_manager.py
+++ pass_manager.py
@@ -1,7 +1,7 @@
 # pass_manager.py
 from inside_storage import pass_storage, get_passwords, delete_password, edit_password, usuari, servei
-from password_encryption import encrypt_password, decrypt_password
-from password_generator import generate_secure_password
+from pass_encryption import encrypt_password, decrypt_password
+from pass_generator import generate_secure_password
 from authentication import authenticate_user
 from gui import display_ui
 
