e9ad95304f5a82b46b84c72f72f94935be2673e0
HamzaEnti <hamza.elhaddad@enti.cat>
2024-12-22 18:00:01 +0100

    bug fix

--- pass_generator.py
+++ pass_generator.py
@@ -1,16 +1,9 @@
-# pass_generator.py
 import secrets
 import string
 
-#Això genera la contrasenya, en cas de que l'usuari no tingui una contrasenya que guardar.
-def pass_generator2():
-    num = int(input("Length:"))# Això marca la llargada de la contrasenya. En principu a la GUI no es pot escollir res que no sigui numero, per lo tant no s'ha de fer error management. 
+def pass_generator2(length):
+    if length <= 0:
+        raise ValueError("La longitud debe ser un número positivo.")
     alfabet = string.ascii_letters + string.digits
-    GENERATEDPASSWORD = "" .join(secrets.choice(alfabet) for char in range(num))
-    return GENERATEDPASSWORD #Aquesta variable no es pot canviar. Aquesta variable s'ha d'encriptar.
-        
-
-  
-
-
-    
+    GENERATEDPASSWORD = "".join(secrets.choice(alfabet) for _ in range(length))
+    return GENERATEDPASSWORD
