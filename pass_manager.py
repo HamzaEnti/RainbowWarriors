e9869d0a88384bb99d0a4d62cab9b57984cee6e7
Nasapasa1 <nicolau.mesalles@enti.cat>
2025-01-09 11:06:34 +0100

    "PASSMNGR-12 <acabat>"

--- pass_manager.py
+++ pass_manager.py
@@ -1,10 +1,25 @@
 # pass_manager.py
-from inside_storage import pass_storage, get_passwords, delete_password, edit_password, usuari, servei
-from pass_encryption import encrypt_password, decrypt_password
-from pass_generator import generate_secure_password
-from authentication import authenticate_user
+from inside_storage import pass_storage, get_passwords, delete_password, edit_password, usuari, servei, decrypt_storage
 from gui import display_ui
+
+
 
 def main():
     # Aquí se puede implementar el flujo principal de la aplicación
-    pass
+    def update():
+        #aqui va tot l'apartat de lògica del programa.
+        get_passwords()
+        edit_password()
+        delete_password()
+        usuari()
+        servei()
+        pass_storage()
+        decrypt_storage()
+        update()
+        
+    def draw():
+        #aqui va tot l'apartat visual del programa. en principi aqui es on s'ajunta la lògica amb la GUI.
+        display_ui()
+        draw
+
+main
