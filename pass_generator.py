5050fa29c20981be8ab2891f09287ddfbac36ebc
Nico <nicolau.mesalles@enti.cat>
2025-01-12 16:12:15 +0100

    PASSMNGR-14

--- pass_generator.py
+++ pass_generator.py
@@ -4,6 +4,7 @@ import string
 def pass_generator2(length):
     if length <= 0:
         raise ValueError("La longitud debe ser un número positivo.")
-    alfabet = string.ascii_letters + string.digits
-    GENERATEDPASSWORD = "".join(secrets.choice(alfabet) for _ in range(length))
+    alfabet = string.asciiletters + string.digits + string.punctuation
+
+    GENERATEDPASSWORD = "".join(secrets.choice(alfabet) for  in range(length))
     return GENERATEDPASSWORD
